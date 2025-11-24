3181fb5a1d761cbc5ad388dda80581329794815f
Jusma <jusmawaty995@gmail.com>
2025-11-24 22:53:15 +0800

    Update analisis_data.py

--- analisis_data.py
+++ analisis_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # Baca data hasil integrasi
-df_integrasi = pd.read_csv("data_integrasi.csv")
+df_integrasi = pd.read_csv("("produk e-commerce.csv")
 
 # Analisis
-produk_per_kategori = df_integrasi['kategori'].value_counts()
-rata_harga = df_integrasi.groupby('kategori')['harga'].mean()
-total_stok = df_integrasi.groupby('kategori')['stok'].sum()
+produk_per_kategori = df_integrasi['Category'].value_counts()
+rata_harga = df_integrasi.groupby('Category')['Price'].mean()
+total_stok = df_integrasi.groupby('Category')['Stock'].sum()
 
 # Simpan hasil analisis ke file
 df_analysis = pd.DataFrame({
@@ -18,3 +18,4 @@ df_analysis = pd.DataFrame({
 df_analysis.to_csv("data_analysis.csv")
 
 print("=== Hasil analisis disimpan di data_analysis.csv ===")
+
