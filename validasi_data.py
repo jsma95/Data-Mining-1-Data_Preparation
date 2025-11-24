a3f85220b9c003f997555ee489bc6eec7b94f8fb
Jusma <jusmawaty995@gmail.com>
2025-11-24 22:54:41 +0800

    Update validasi_data.py

--- validasi_data.py
+++ validasi_data.py
@@ -1,16 +1,17 @@
 import pandas as pd
 
 # Baca data hasil integrasi
-df_integrasi = pd.read_csv("data_integrasi.csv")
+df_integrasi = pd.read_csv("produk e-commerce.csv")
 
 # Validasi
 # Hapus duplikat
 df_validasi = df_integrasi.drop_duplicates()
 
-# Hapus data dengan harga <= 0 atau stok < 0
-df_validasi = df_validasi[(df_validasi['harga'] > 0) & (df_validasi['stok'] >= 0)]
+# Hapus data dengan Price <= 0 atau Stock < 0
+df_validasi = df_validasi[(df_validasi['Price'] > 0) & (df_validasi['Stock'] >= 0)]
 
 # Simpan hasil validasi
 df_validasi.to_csv("data_validation.csv", index=False)
 
 print("=== Data setelah validasi disimpan di data_validation.csv ===")
+
